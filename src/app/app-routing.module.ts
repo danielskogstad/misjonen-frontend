1c96bf0e4e494d5654a0667370f719119bf8e259
danielskogstad <daniel.skogstad@gmail.com>
2019-07-08 13:42:28 +0200

    Added routes, main template and module refs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PodcastListComponent } from './components/podcast-list/podcast-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/podcast/misjonen/list', pathMatch: 'full'},
+  {path: '**', redirectTo: '/podcast/misjonen/list'},
+  {path: 'podcast/misjonen/list', component: PodcastListComponent},
+  {path: '**', redirectTo: '/podcast/misjonen/list'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './components/header/header.component';
+import { PodcastListComponent } from './components/podcast-list/podcast-list.component';
+
+import { P4PodcastService } from './services/p4podcast.service';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PodcastListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [
+    P4PodcastService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
