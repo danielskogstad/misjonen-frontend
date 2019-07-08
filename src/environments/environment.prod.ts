01ad5a81114bc6ba130793c4bfd8d91b2114e0bb
danielskogstad <daniel.skogstad@gmail.com>
2019-07-08 13:41:06 +0200

    Environment and material module

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  p4podcastUrl: 'https://api.p4.no/1/data/lydklipp',
+  misjonenSectionId: 423
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  p4podcastUrl: 'https://api.p4.no/1/data/lydklipp',
+  misjonenSectionId: 423
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
